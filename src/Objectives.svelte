b9eaf40204f00ba3ec8b84babb0a05c25aa0c75f
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 17:55:44 +0800

    Update objective component style to be card

--- src/Objectives.svelte
+++ src/Objectives.svelte
@@ -10,13 +10,18 @@
 </script>
 
 <div class="mb-8">
-  <h2 class="font-bold text-xl mb-2">Objectives <span class="opacity-50"> ~ 2020</span></h2>
+  <div class="bg-nord1 inline-flex flex-col p-6 rounded-lg shadow-lg">
+    <h2 class="font-bold text-xl mb-2">
+      Objectives
+      <span class="opacity-50">~ 2020</span>
+    </h2>
 
-  <ol class="list-decimal list-inside">
-    {#each objectives as objective}
-      <li class="leading-loose">
-        {@html objective}
-      </li>
-    {/each}
-  </ol>
+    <ol class="list-decimal list-inside">
+      {#each objectives as objective}
+        <li class="leading-loose">
+          {@html objective}
+        </li>
+      {/each}
+    </ol>
+  </div>
 </div>
