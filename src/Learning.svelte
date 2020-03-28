809b6c94bb0fbd59aebc86774a46e279db36ee14
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 17:56:06 +0800

    Add new learning component

---
+++ src/Learning.svelte
@@ -0,0 +1,11 @@
+<div class="block">
+  <h2 class="font-bold text-lg mb-4">Continuous Learning</h2>
+  <ul>
+    <li class="text-nord7 text-sm">
+      <a
+        href="https://www.coursera.org/learn/golang-getting-started/home/welcome">
+        Coursera - Getting Started with Go
+      </a>
+    </li>
+  </ul>
+</div>
