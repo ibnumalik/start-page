658f75e64c43ca8b6ea5985e60f6d5cc9651e98b
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 17:56:18 +0800

    Update header date styling

--- src/Header.svelte
+++ src/Header.svelte
@@ -4,6 +4,6 @@
 
 <div class="header pt-12 pb-16">
   <hr class="mb-20 opacity-25" />
-  <p class="text-sm uppercase mb-2">Saturday, 28 March 2020</p>
+  <p class="text-sm uppercase mb-2 opacity-75">Saturday, 28 March 2020</p>
   <h1 class="text-4xl font-bold">Hello {name}!</h1>
 </div>
