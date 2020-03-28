c5379805518cc26b6d1855fab99a24a09ccd3329
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-29 07:38:53 +0800

    Use dynamic date from javascript

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,9 +1,19 @@
 <script>
   export let name;
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+  const d = new Date();
+
+  const date = d.toLocaleDateString('en-MY', options);
 </script>
 
 <div class="header pt-12 pb-16">
   <hr class="mb-20 opacity-25" />
-  <p class="text-sm uppercase mb-2 opacity-75">Saturday, 28 March 2020</p>
+  <p class="text-sm uppercase mb-2 opacity-75">{date}</p>
   <h1 class="text-4xl font-bold">Hello {name}!</h1>
 </div>
