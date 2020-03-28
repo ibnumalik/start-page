23ce73259ee25889c4c9a6b2906c41a26c0bbc4b
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 18:45:13 +0800

    Load learning data from json

--- src/Learning.svelte
+++ src/Learning.svelte
@@ -1,11 +1,26 @@
+<script>
+  import { onMount } from "svelte";
+
+  let educations = [];
+
+  onMount(async () => {
+    const res = await fetch("/data/learning.json");
+    educations = await res.json();
+  });
+</script>
+
 <div class="block">
   <h2 class="font-bold text-lg mb-4">Continuous Learning</h2>
   <ul>
-    <li class="text-nord7 text-sm">
-      <a
-        href="https://www.coursera.org/learn/golang-getting-started/home/welcome">
-        Coursera - Getting Started with Go
-      </a>
-    </li>
+    {#each educations as education}
+      <li class="mb-2 max-w-18">
+        <a class="bg-nord2 px-4 py-3 rounded-md flex flex-col items-start" href={education.link}>
+          <span class="mb-4 font-bold">{education.subject}</span>
+          <span class="bg-nord9 rounded text-sm px-1  text-nord0">
+            {education.academy}
+          </span>
+        </a>
+      </li>
+    {/each}
   </ul>
 </div>
