e26bf873c8a1a1a4a9b36db7adb7b8865f9b5c52
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 13:23:33 +0800

    Add basic structure for main page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,32 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-</main>
-
 <style>
+
 </style>
+
+<main class="min-h-screen bg-nord0 text-nord4 font-sans">
+  <div class="container mx-auto">
+    <div class="header pt-12 pb-16">
+      <hr class="mb-20 opacity-25" />
+      <p class="text-sm uppercase mb-2">Saturday, 28 March 2020</p>
+      <h1 class="text-4xl font-bold">Hello {name}!</h1>
+    </div>
+
+    <div class="flex">
+      <section class="learning">
+        <h2 class="font-bold text-lg mb-4">Continuous Learning</h2>
+        <ul>
+          <li class="text-nord7 text-sm">
+            <a
+              href="https://www.coursera.org/learn/golang-getting-started/home/welcome">
+              Coursera - Getting Started with Go
+            </a>
+          </li>
+        </ul>
+      </section>
+
+    </div>
+  </div>
+</main>
