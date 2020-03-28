7f0b189993493c31a07f07daf7e3f3819282a2ad
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 18:44:48 +0800

    Add projects component

---
+++ src/Projects.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  let projects = [];
+
+  onMount(async () => {
+    const res = await fetch("/data/learning.json");
+    projects = await res.json();
+  });
+</script>
+
+<div class="block">
+  <h2 class="font-bold text-lg mb-4">Projects</h2>
+  <ul>
+    {#each projects as education}
+      <li class="mb-2">
+        <a class="bg-nord2 p-2" href={education.link}>{education.academy} - {education.subject}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
