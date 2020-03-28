e40685c15ec7ba06d104e3b51bc37e75aa8cd9e9
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 15:50:57 +0800

    Add objective component

---
+++ src/Objectives.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from "svelte";
+  let objectives = [];
+
+  onMount(async () => {
+    const res = await fetch(`./data/objectives.json`);
+    const data = await res.json();
+    objectives = data.objectives;
+  });
+</script>
+
+<div class="mb-8">
+  <h2 class="font-bold text-xl mb-2">Objectives <span class="opacity-50"> ~ 2020</span></h2>
+
+  <ol class="list-decimal list-inside">
+    {#each objectives as objective}
+      <li class="leading-loose">
+        {@html objective}
+      </li>
+    {/each}
+  </ol>
+</div>
