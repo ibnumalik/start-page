32b190610a5c8508aa53f100b79a53cdfc0d9e60
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 15:50:34 +0800

    Add header component

---
+++ src/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let name;
+</script>
+
+<div class="header pt-12 pb-16">
+  <hr class="mb-20 opacity-25" />
+  <p class="text-sm uppercase mb-2">Saturday, 28 March 2020</p>
+  <h1 class="text-4xl font-bold">Hello {name}!</h1>
+</div>
