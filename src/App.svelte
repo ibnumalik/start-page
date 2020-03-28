5a20b114e8fe636237275362dc08ba3bef9265b0
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-03-28 18:03:29 +0800

    Use header, objective component in App component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,9 @@
 <script>
-  export let name;
+  import Header from "./Header.svelte";
+  import Learning from "./Learning.svelte";
+  import Objectives from "./Objectives.svelte";
+
+  const name = "Shaiful Aiman";
 </script>
 
 <style>
@@ -8,25 +12,18 @@
 
 <main class="min-h-screen bg-nord0 text-nord4 font-sans">
   <div class="container mx-auto">
-    <div class="header pt-12 pb-16">
-      <hr class="mb-20 opacity-25" />
-      <p class="text-sm uppercase mb-2">Saturday, 28 March 2020</p>
-      <h1 class="text-4xl font-bold">Hello {name}!</h1>
-    </div>
+    <Header {name} />
+
+    <Objectives />
 
     <div class="flex">
-      <section class="learning">
-        <h2 class="font-bold text-lg mb-4">Continuous Learning</h2>
-        <ul>
-          <li class="text-nord7 text-sm">
-            <a
-              href="https://www.coursera.org/learn/golang-getting-started/home/welcome">
-              Coursera - Getting Started with Go
-            </a>
-          </li>
-        </ul>
+      <section class="flex-auto">
+        <Learning />
       </section>
 
+      <section class="flex-auto">
+        <Learning />
+      </section>
     </div>
   </div>
 </main>
