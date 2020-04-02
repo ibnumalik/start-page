2f0f2a9e68ecf52896e70247137182b6116487bb
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-04-02 17:11:00 +0800

    Open learning website in new tab

--- src/Learning.svelte
+++ src/Learning.svelte
@@ -1,10 +1,10 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount } from 'svelte';
 
   let educations = [];
 
   onMount(async () => {
-    const res = await fetch("/data/learning.json");
+    const res = await fetch('/data/learning.json');
     educations = await res.json();
   });
 </script>
@@ -13,16 +13,18 @@
   <h2 class="font-bold text-lg mb-4 text-nord6">Continuous Learning</h2>
   <ul class="-mx-2">
     {#each educations as education}
-      <li class="mb-2">
-        <a
-          class="px-2 py-3 rounded-md flex flex-col items-start max-w-18 hover:bg-nord2"
-          href={education.link}>
-          <span class="mb-4 font-bold">{education.subject}</span>
-          <span class="bg-nord9 rounded text-sm px-1 text-nord0">
-            {education.academy}
-          </span>
-        </a>
-      </li>
+    <li class="mb-2">
+      <a
+        target="_blank"
+        class="px-2 py-3 rounded-md flex flex-col items-start max-w-18 hover:bg-nord2"
+        href="{education.link}"
+      >
+        <span class="mb-4 font-bold">{education.subject}</span>
+        <span class="bg-nord9 rounded text-sm px-1 text-nord0">
+          {education.academy}
+        </span>
+      </a>
+    </li>
     {/each}
   </ul>
 </div>
