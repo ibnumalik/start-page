b49b08cf34fed5266da137cdd8637418491396c2
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-06-22 11:21:55 +0800

    Add edit mode for objectives

--- src/Objectives.svelte
+++ src/Objectives.svelte
@@ -1,26 +1,42 @@
 <script>
-  import { onMount } from "svelte";
-  let objectives = [];
+  import { onMount } from 'svelte';
+  import { objectives } from './stores';
+  let editMode = false;
+  $: editButtonText = editMode ? 'done' : 'edit';
 
-  onMount(async () => {
-    const res = await fetch(`./data/objectives.json`);
-    objectives = await res.json();
-  });
+  function toggleEdit() {
+    editMode = !editMode;
+  }
 </script>
 
 <div class="mb-8">
-  <div class="bg-nord1 inline-flex flex-col p-6 rounded-lg shadow-lg">
-    <h2 class="font-bold text-xl mb-2">
-      Objectives
-      <span class="opacity-50">~ 2020</span>
-    </h2>
+  <div class="inline-flex flex-col p-6 rounded-lg shadow-lg bg-nord1">
+    <div class="flex items-center justify-between mb-2">
+      <h2 class="text-xl font-bold">
+        Objectives
+        <span class="opacity-50">~ 2020</span>
+      </h2>
 
+      <button
+        class="px-2 text-sm text-opacity-75 rounded hover:bg-nord10 hover:text-nord6 text-nord10"
+        on:click="{toggleEdit}"
+      >
+        {editButtonText}
+      </button>
+    </div>
+
+    {#if $objectives}
     <ol class="list-decimal list-inside">
-      {#each objectives as objective}
-        <li class="leading-loose">
-          {@html objective}
-        </li>
+      {#each $objectives as objective}
+      <li class="mb-2 leading-loose rounded-lg w-500">
+        {#if editMode}
+        <input type="text" bind:value="{objective}" class="w-11/12 bg-nord1" />
+        {:else}
+        <span> {@html objective} </span>
+        {/if}
+      </li>
       {/each}
     </ol>
+    {/if}
   </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: ['./src/**/*.svelte'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '500': '500px',
+      },
+    },
     fontFamily: {
       sans: [
         '-apple-system',
