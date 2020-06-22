44e17f8236b88c0729fcba1bd630cba8a73a4e32
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-06-22 11:39:09 +0800

    Add ability to add objectives

--- src/Objectives.svelte
+++ src/Objectives.svelte
@@ -1,21 +1,39 @@
 <script>
   import { onMount } from 'svelte';
   import { objectives } from './stores';
+  import { Storage } from './storage';
   let editMode = false;
   $: editButtonText = editMode ? 'done' : 'edit';
 
   function toggleEdit() {
+    if (editMode) {
+      Storage.save('objectives', $objectives);
+    }
+
     editMode = !editMode;
+  }
+
+  function add() {
+    objectives.update((objectives$) => [...objectives$, '']);
   }
 </script>
 
 <div class="mb-8">
   <div class="inline-flex flex-col p-6 rounded-lg shadow-lg bg-nord1">
     <div class="flex items-center justify-between mb-2">
-      <h2 class="text-xl font-bold">
+      <h2 class="mr-auto text-xl font-bold">
         Objectives
         <span class="opacity-50">~ 2020</span>
       </h2>
+
+      {#if editMode}
+      <button
+        class="px-2 text-sm text-opacity-75 rounded text-nord14"
+        on:click="{add}"
+      >
+        add
+      </button>
+      {/if}
 
       <button
         class="px-2 text-sm text-opacity-75 rounded hover:bg-nord10 hover:text-nord6 text-nord10"
