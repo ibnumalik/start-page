20d022c29ceb12dbccbb187697dc3ad6d6b33054
Shaiful Aiman Malik <ibnumalik@tbd.my>
2020-06-24 11:14:03 +0800

    Add export base function

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,16 +1,36 @@
 <script>
   const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric"
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   };
   const d = new Date();
 
-  const date = d.toLocaleDateString("en-MY", options);
+  const date = d.toLocaleDateString('en-MY', options);
+
+  function exports() {
+    const rows = [
+      ['whatev', 'dontcare', 'success'],
+    ];
+
+    let csvContent =
+      'data:application/json;charset=utf-8,' + rows.map((e) => e.join(',')).join('\n');
+
+    const encodedContent = encodeURI(csvContent);
+    window.open(encodedContent);
+  }
 </script>
 
-<div class="header pt-12 pb-8">
+<div class="pt-12 pb-8 header">
   <p class="opacity-50">Today is</p>
-  <p class="text-5xl">{date}</p>
+  <div class="flex items-center justify-between">
+    <p class="text-5xl">{date}</p>
+    <button
+      on:click="{exports}"
+      class="text-opacity-25 hover:text-opacity-75 text-nord9"
+    >
+      export
+    </button>
+  </div>
 </div>
